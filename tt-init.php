ffa05486302c88e63bf08cbbe838e640eaf2952c
lukehayes <lukepurplehayes@gmail.com>
2018-03-26 15:38:24 +0100

    -------------- diff --git a/tt-init.php b/tt-init.php index e69de29..b5db153 100644 --- a/tt-init.php +++ b/tt-init.php @@ -0,0 +1,11 @@ +<?php +/* +Plugin Name: Themetacular Portfolio +Plugin URI: https://www.themetacular.com/plugins/tt-portfolio +Description: Portfolio functionality for Themetacular Wordpress themes. +Version: 0.1.0 +Author: Themetacular +Author URI: http://www.themetacular.com +Text Domain: portfolio +Domain Path: / +*/ \ No newline at end of file

---
+++ tt-init.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Themetacular Portfolio
+Plugin URI: https://www.themetacular.com/plugins/tt-portfolio
+Description: Portfolio functionality for Themetacular Wordpress themes.
+Version: 0.1.0
+Author: Themetacular
+Author URI: http://www.themetacular.com
+Text Domain: portfolio
+Domain Path: /
+*/
