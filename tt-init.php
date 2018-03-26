6363390862945bf234b99aa864975c2d89c18ef1
lukehayes <lukepurplehayes@gmail.com>
2018-03-26 15:38:46 +0100

    Added plugin header

--- tt-init.php
+++ tt-init.php
@@ -6,6 +6,6 @@ Description: Portfolio functionality for Themetacular Wordpress themes.
 Version: 0.1.0
 Author: Themetacular
 Author URI: http://www.themetacular.com
-Text Domain: portfolio
+Text Domain: Portfolio
 Domain Path: /
 */
