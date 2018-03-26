838d14d292b1617a56bec99aa2b69ad22ecd0c5c
lukehayes <lukepurplehayes@gmail.com>
2018-03-26 16:55:07 +0100

    Updated metabox markup

--- tt-portfolio-metabox.php
+++ tt-portfolio-metabox.php
@@ -15,11 +15,13 @@ function tt_build_metabox_html() {
     ?>
     <div class="inside">
         <p>
+            <p class="howto">Additional information about the client, like a name/brand.</p>
             <label for="tt_portfolio_mb_client">Client: </label>
             <input type="text" name="tt_portfolio_mb_client" placeholder="The name of the client the work is for perhaps?">
         </p>
 
         <p>
+            <p class="howto">When the work was completed.</p>
             <label for="tt_portfolio_mb_date">Date: </label>
             <input type="text" name="tt_portfolio_mb_date" placeholder="Could be year, month etc...">
         </p>
