adea3e12be3da13a18f8152146f4cd74a2b36a3a
lukehayes <lukepurplehayes@gmail.com>
2018-03-26 16:35:55 +0100

    Changed Attributes to Information

--- tt-portfolio-metabox.php
+++ tt-portfolio-metabox.php
@@ -7,7 +7,7 @@
  * Add the portfolio metabox the the custom post type
  */
 function tt_add_portfolio_metabox() {
-    add_meta_box( 'tt_portfolio_metabox', 'Portfolio Attributes', 'tt_build_metabox_html', 'portfolio', $context = 'advanced', $priority = 'default', $callback_args = null );
+    add_meta_box( 'tt_portfolio_metabox', 'Portfolio Information', 'tt_build_metabox_html', 'portfolio', $context = 'advanced', $priority = 'default', $callback_args = null );
 }
 add_action('add_meta_boxes', 'tt_add_portfolio_metabox');
 
