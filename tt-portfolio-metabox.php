b9f444455ce343e9ed47f8e189eb3f2c8c465cb1
lukehayes <lukepurplehayes@gmail.com>
2018-03-27 16:56:33 +0100

    Remover duplicate #inside div

--- tt-portfolio-metabox.php
+++ tt-portfolio-metabox.php
@@ -20,18 +20,16 @@ add_action('add_meta_boxes', 'tt_add_portfolio_metabox');
 
 function tt_build_metabox_html( $post ) {
     ?>
-    <div class="inside">
-        <p>
-            <p class="howto">Additional information about the client, like a name/brand.</p>
-            <label for="tt_portfolio_mb_client">Client: </label>
-            <input type="text" name="tt_portfolio_mb_client" class="regular-text" placeholder="The name of the client the work is for perhaps?">
-        </p>
+    <p>
+        <p class="howto">Additional information about the client, like a name/brand.</p>
+        <label for="tt_portfolio_mb_client">Client: </label>
+        <input type="text" name="tt_portfolio_mb_client" class="regular-text" placeholder="The name of the client the work is for perhaps?">
+    </p>
 
-        <p>
-            <p class="howto">When the work was completed.</p>
-            <label for="tt_portfolio_mb_date">Date: </label>
-            <input type="text" name="tt_portfolio_mb_date" class="regular-text" placeholder="Could be year, month etc...">
-        </p>
-    </div>
+    <p>
+        <p class="howto">When the work was completed.</p>
+        <label for="tt_portfolio_mb_date">Date: </label>
+        <input type="text" name="tt_portfolio_mb_date" class="regular-text" placeholder="Could be year, month etc...">
+    </p>
     <?php
 }
