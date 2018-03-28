065f8f91e2fa5a586493ecdd8b1a892c6c78ccfc
lukehayes <lukepurplehayes@gmail.com>
2018-03-28 15:21:42 +0100

    Added extra comments

--- tt-portfolio-metabox.php
+++ tt-portfolio-metabox.php
@@ -21,6 +21,8 @@ add_action('add_meta_boxes', 'tt_add_portfolio_metabox');
 function tt_build_metabox_html( $post ) {
         var_dump($_POST);
         var_dump($_REQUEST);
+        var_dump(get_post_meta('tt_portfolio_mb_client'));
+        var_dump($_POST['tt_portfolio_mb_client']);
     ?>
     <table class="form-table">
         <tr>
@@ -28,9 +30,8 @@ function tt_build_metabox_html( $post ) {
         </tr>
         <tr>
             <th class="row-title">
-                <?php wp_nonce_field( basename( __FILE__ ), 'tt_portfolio_mb_client' ); ?>
                 <label for="tt_portfolio_mb_client">Client: </label>
-                <input type="text" name="tt_portfolio_mb_client" class="regular-text" placeholder="The name of the client the work is for perhaps?">
+                <input type="text" id="tt_portfolio_mb_client" name="tt_portfolio_mb_client" class="regular-text" placeholder="The name of the client the work is for perhaps?">
             </th>
         </tr>
 
@@ -47,5 +48,14 @@ function tt_build_metabox_html( $post ) {
 }
 
 function tt_save_mb_values($post_id, $post) {
+
+    // Is the post an Auto Draft?
+    if( isset($post->post_status) && 'auto-draft' == $post->post_status ) return;
+
+    // Are we in the middle of autosating?
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+
+    // Is the current user allowed here?
+    if ( ! current_user_can( 'edit_post', $post_id ) ) return;
 }
-add_action('save_post_portfolio', 'tt_save_mb_values', 10, 2);
+add_action('save_post', 'tt_save_mb_values', 10, 2);
