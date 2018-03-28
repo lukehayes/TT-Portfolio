c943c2f8fb0d68668df1239405a7310a31f2ad75
lukehayes <lukepurplehayes@gmail.com>
2018-03-28 16:30:33 +0100

    Updated code

--- tt-portfolio-metabox.php
+++ tt-portfolio-metabox.php
@@ -19,11 +19,11 @@ function tt_add_portfolio_metabox() {
 add_action('add_meta_boxes', 'tt_add_portfolio_metabox');
 
 function tt_build_metabox_html( $post ) {
+        var_dump(get_post_meta($post->ID, 'tt_portfolio_mb_client'));
         var_dump($_POST);
-        var_dump($_REQUEST);
-        var_dump(get_post_meta('tt_portfolio_mb_client'));
-        var_dump($_POST['tt_portfolio_mb_client']);
     ?>
+
+    <?php wp_nonce_field( basename( __FILE__ ), 'tt_portfolio_mb_client_nonce' ); ?>
     <table class="form-table">
         <tr>
             <h4>Add additonal information about your portfolio item here:</h4>
@@ -37,7 +37,6 @@ function tt_build_metabox_html( $post ) {
 
         <tr>
             <th class="row-title">
-                <?php wp_nonce_field( basename( __FILE__ ), 'tt_portfolio_mb_client' ); ?>
                 <label for="tt_portfolio_mb_date">Date: </label>
                 <input type="text" name="tt_portfolio_mb_date" class="regular-text" placeholder="Could be year, month etc...">
             </th>
@@ -48,14 +47,36 @@ function tt_build_metabox_html( $post ) {
 }
 
 function tt_save_mb_values($post_id, $post) {
-
     // Is the post an Auto Draft?
-    if( isset($post->post_status) && 'auto-draft' == $post->post_status ) return;
+    // if( ! isset($post->post_status) && 'auto-draft' == $post->post_status ) return;
+
+    update_post_meta( $post_id, 'tt_portfolio_mb_client', 'test', $prev_value = 'sssss' );
 
     // Are we in the middle of autosating?
     if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+    var_dump('two');
 
     // Is the current user allowed here?
     if ( ! current_user_can( 'edit_post', $post_id ) ) return;
+    var_dump('three');
+
+    // Has the input value been set?
+    if( isset($_POST['tt_portfolio_mb_client']) ) {
+        update_post_meta( $post_id, 'tt_portfolio_mb_client', $_POST['tt_portfolio_mb_client'], $prev_value = 'sssss' );
+    }else {
+        var_dump("Nope");
+        return;
+    }
+    var_dump('four');
+
+    // Check the nonce
+    if ( empty($_POST[tt_portfolio_mb_client_nonce]) || ! wp_verify_nonce( $_POST[tt_portfolio_mb_client_nonce], basename(__FILE__) ) ) return;
+    var_dump('five');
+
+    var_dump("ooooooooooooooooooooo");
+    var_dump("ooooooooooooooooooooo");
+    var_dump("ooooooooooooooooooooo");
+    var_dump("ooooooooooooooooooooo");
+
 }
-add_action('save_post', 'tt_save_mb_values', 10, 2);
+add_action('save_post_portfolio', 'tt_save_mb_values', 10, 2);
