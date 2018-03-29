0bee2893638416656e981f354f6765d924fadaf1
lukehayes <lukepurplehayes@gmail.com>
2018-03-29 16:38:30 +0100

    $_POST now working

--- tt-portfolio-metabox.php
+++ tt-portfolio-metabox.php
@@ -6,30 +6,34 @@
 
 $inputs = [
     'client' => 'tt_portfolio_mb_client',
-    'date' => 'tt_portfolio_mb_date',
+    'date' => 'tt_portfolio_mb_date'
 ];
 
 /**
  * Add the portfolio metabox the the custom post type
  */
-function tt_add_portfolio_metabox() {
+function tt_add_portfolio_metabox($inputs) {
+    global $inputs;
     add_meta_box(
         'tt_portfolio_metabox',
         'Portfolio Information',
         'tt_build_metabox_html',
         'portfolio',
         $context = 'normal',
-        $priority = 'high',
-        $callback_args = null );
+        $priority = 'high');
 }
-add_action('add_meta_boxes', 'tt_add_portfolio_metabox');
+add_action('add_meta_boxes', 'tt_add_portfolio_metabox', 10, 2);
 
-function tt_build_metabox_html( $post ) {
-        var_dump(get_post_meta($post->ID, 'tt_portfolio_mb_client'));
+function tt_build_metabox_html( $post, $inputs ) {
+    global $inputs;
+        echo "Meta";
+        var_dump(get_post_meta($post->ID, $inputs['client'] ));
+        var_dump(get_post_meta($post->ID, $inputs['date'] ));
+        echo "Post <br/>";
         var_dump($_POST);
+        var_dump($_REQUEST);
     ?>
 
-    <?php wp_nonce_field( basename( __FILE__ ), 'tt_portfolio_mb_client_nonce' ); ?>
     <table class="form-table">
         <tr>
             <h4>Add additonal information about your portfolio item here:</h4>
@@ -37,52 +41,44 @@ function tt_build_metabox_html( $post ) {
         <tr>
             <th class="row-title">
                 <label for="tt_portfolio_mb_client">Client: </label>
-                <input type="text" id="tt_portfolio_mb_client" name="tt_portfolio_mb_client" class="regular-text" placeholder="The name of the datdatework is for perhaps?">
+                <input type="text" id="<?php echo $inputs['client'] ?>" name="<?php echo $inputs['client'] ?>" class="regular-text" placeholder="The name of the datdatework is for perhaps?" value="<?php echo get_post_meta($post->ID, $inputs['client'], true); ?>">
             </th>
         </tr>
-
         <tr>
             <th class="row-title">
-                <label for="tt_portfolio_mb_date">Date: </label>
-                <input type="text" name="tt_portfolio_mb_date" class="regular-text" placeholder="Could be year, month etc...">
+                <label for="tt_portfolio_mb_client">Date: </label>
+                <input type="text" id="<?php echo $inputs['date'] ?>" name="<?php echo $inputs['date'] ?>" class="regular-text" placeholder="When the work was completed" value="<?php echo get_post_meta($post->ID, $inputs['date'], true); ?>">
             </th>
         </tr>
-
     </table>
     <?php
 }
 
-function tt_save_mb_values($post_id, $post) {
-    // Is the post an Auto Draft?
-    // if( ! isset($post->post_status) && 'auto-draft' == $post->post_status ) return;
+function tt_save_mb_values($post_id, $post, $inputs) {
 
-    update_post_meta( $post_id, 'tt_portfolio_mb_client', 'test', $prev_value = 'sssss' );
+    global $inputs;
+    // Is the post an Auto Draft?
+    if( ! isset($post->post_status) && 'auto-draft' == $post->post_status ) return;
 
     // Are we in the middle of autosating?
     if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-    var_dump('two');
 
     // Is the current user allowed here?
     if ( ! current_user_can( 'edit_post', $post_id ) ) return;
-    var_dump('three');
 
     // Has the input value been set?
-    if( isset($_POST['tt_portfolio_mb_client']) ) {
-        update_post_meta( $post_id, 'tt_portfolio_mb_client', $_POST['tt_portfolio_mb_client'], $prev_value = 'sssss' );
-    }else {
-        var_dump("Nope");
-        return;
+    if( isset($_POST[ $inputs['client'] ]) ) {
+        update_post_meta( $post_id, $inputs['client'], $_POST[ $inputs['client'] ] );
     }
-    var_dump('four');
+
+    // Has the input value been set?
+    if( isset($_POST[ $inputs['date'] ]) ) {
+        update_post_meta( $post_id, $inputs['date'], $_POST[ $inputs['date'] ] );
+    }
 
     // Check the nonce
-    if ( empty($_POST[tt_portfolio_mb_client_nonce]) || ! wp_verify_nonce( $_POST[tt_portfolio_mb_client_nonce], basename(__FILE__) ) ) return;
-    var_dump('five');
+    // if ( empty($_POST[tt_portfolio_mb_client_nonce]) || ! wp_verify_nonce( $_POST[tt_portfolio_mb_client_nonce], basename(__FILE__) ) ) return;
 
-    var_dump("ooooooooooooooooooooo");
-    var_dump("ooooooooooooooooooooo");
-    var_dump("ooooooooooooooooooooo");
-    var_dump("ooooooooooooooooooooo");
-
+    // if ( ! array_key_exists($inputs['client'], $_POST)) return;
 }
-add_action('save_post_portfolio', 'tt_save_mb_values', 10, 2);
+add_action('save_post_portfolio', 'tt_save_mb_values', 10, 3);
