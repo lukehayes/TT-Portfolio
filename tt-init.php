6143860259da6f2016a584f50c50aa7fedb766d0
lukehayes <lukepurplehayes@gmail.com>
2018-04-04 12:19:50 +0100

    Cleaed tt_portfolio_load_tpl()

--- tt-init.php
+++ tt-init.php
@@ -16,33 +16,17 @@ require plugin_dir_path( __FILE__ ) . 'tt-portfolio-cpt.php';
 /* Portfolio Metabox ----------------------------------------------*/
 require plugin_dir_path( __FILE__ ) . 'tt-portfolio-metabox.php';
 
-function tt_portfolio_load_tpl( $template ) { 
-
-    $file = plugin_dir_path(__FILE__) . 'templates/portfolio-template.php';
-    $template = $file;
-    var_dump($template);
-    var_dump(locate_template(plugin_dir_path(__FILE__) . 'templates/portfolio-template.php', true));
-    return $template;
-
-    // if ( is_singular('portfolio') ) {
-    //     var_dump("Is Single");
-    //     $template =  locate_template(plugin_dir_path(__FILE__) . 'tempaltes/portfolio-template.php');
-    // }else {
-    //     var_dump("Is not");
-    //     return $template;
-    // }
-
-
-    // return $template;
-
-    // if ( is_singular('Portfolio') ) {
-    //     var_dump("YEs");
-    //     $template = locate_template( plugin_dir_path(__FILE__) . 'templates/portfolio-template.php' );
-    //     return $template;
-    // }else {
-    //     var_dump("NO");
-    //     return $template;
-    // }
+/**
+ * Load the template for the Portfolio CPT
+ */
+function tt_portfolio_load_tpl( $template ) {
+    if ( is_singular( 'portfolio') ) {
+        // Get the template path
+        $template = plugin_dir_path(__FILE__) . 'templates/portfolio-template.php';
+        return $template;
+    }else {
+        return $template;
+    }
 
 }
 add_action('template_include', 'tt_portfolio_load_tpl',1);
