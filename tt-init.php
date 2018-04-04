cafdf915351542cefa14e7e70ee113b10956e4e2
lukehayes <lukepurplehayes@gmail.com>
2018-04-04 12:23:18 +0100

    Added TT_Templater class

--- tt-init.php
+++ tt-init.php
@@ -16,17 +16,5 @@ require plugin_dir_path( __FILE__ ) . 'tt-portfolio-cpt.php';
 /* Portfolio Metabox ----------------------------------------------*/
 require plugin_dir_path( __FILE__ ) . 'tt-portfolio-metabox.php';
 
-/**
- * Load the template for the Portfolio CPT
- */
-function tt_portfolio_load_tpl( $template ) {
-    if ( is_singular( 'portfolio') ) {
-        // Get the template path
-        $template = plugin_dir_path(__FILE__) . 'templates/portfolio-template.php';
-        return $template;
-    }else {
-        return $template;
-    }
-
-}
-add_action('template_include', 'tt_portfolio_load_tpl',1);
+/* Portfolio Templater Class --------------------------------------*/
+require plugin_dir_path( __FILE__ ) . 'tt-templater-class.php';
---
+++ tt-templater-class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class for managing the template files for the Portfolio Plugin
+ */
+
+class TT_Templater {
+
+    public function __construct() {
+        add_action('template_include', array($this, 'tt_portfolio_load_tpl') ,1);
+    }
+
+    /**
+     * Load the template for the Portfolio CPT
+     */
+    function tt_portfolio_load_tpl( $template ) {
+        if ( is_singular( 'portfolio') ) {
+            // Get the template path
+            $template = plugin_dir_path(__FILE__) . 'templates/portfolio-template.php';
+            return $template;
+        }else {
+            return $template;
+        }
+    }
+}
