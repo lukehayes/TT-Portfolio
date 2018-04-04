4ed79a7f3be6debb60ae7e95e49a457c09d3808b
lukehayes <lukepurplehayes@gmail.com>
2018-04-04 14:17:56 +0100

    Cleaned portfolio-template

--- templates/portfolio-template.php
+++ templates/portfolio-template.php
@@ -6,13 +6,7 @@ Template Post Type: product
 ?>
 
 <?php get_header(); ?>
-<h3>Portfolio Template from plugin.</h3>
-
-
-<?php global $post; ?>
 
 <p><?php the_title(); ?></p>
-
-<p><?php echo $post->post_content; ?></p>
 
 <?php get_footer(); ?>
