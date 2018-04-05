c1a0d48843d7c80cc8b5c6309487266a4415a44d
lukehayes <lukepurplehayes@gmail.com>
2018-04-05 12:54:37 +0100

    Added admin styles hook

--- tt-init.php
+++ tt-init.php
@@ -18,3 +18,11 @@ require plugin_dir_path( __FILE__ ) . 'tt-portfolio-metabox.php';
 
 /* Portfolio Templater Class --------------------------------------*/
 require plugin_dir_path( __FILE__ ) . 'tt-templater-class.php';
+
+/* Load the admin CSS styles --------------------------------------*/
+function tt_portfolio_admin_css() {
+    $path = plugin_dir_path(__FILE__) . 'styles/tt-portfolio-admin-style.css';
+    wp_register_style( 'tt-portfolio-admin-css', $path, array(), false, $media = 'all' );
+    wp_enqueue_style( 'tt-portfolio-admin-css');
+}
+add_action('admin_init', 'tt_portfolio_admin_css');
